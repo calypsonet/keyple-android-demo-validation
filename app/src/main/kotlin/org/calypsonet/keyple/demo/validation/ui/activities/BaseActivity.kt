cb7139c559018313d60c8ab2cb8208584610ff8b
Youssef Amrani <youssef.amrani@yamapps.fr>
2021-06-14 17:57:36 +0200

    Update package name : eclipse -> calypsonet
    Signed-off-by: Youssef Amrani <youssef.amrani@yamapps.fr>

---
+++ app/src/main/kotlin/org/calypsonet/keyple/demo/validation/ui/activities/BaseActivity.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021 Calypso Networks Association https://www.calypsonet-asso.org/
+ *
+ * See the NOTICE file(s) distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package org.calypsonet.keyple.demo.validation.ui.activities
+
+import dagger.android.support.DaggerAppCompatActivity
+import org.calypsonet.keyple.demo.validation.data.LocationFileManager
+import javax.inject.Inject
+
+/**
+ *  @author youssefamrani
+ */
+
+abstract class BaseActivity : DaggerAppCompatActivity() {
+
+    @Inject
+    lateinit var locationFileManager: LocationFileManager
+
+}
---
+++ app/src/test/kotlin/org/calypsonet/keyple/demo/validation/mock/MockSamReaderImpl.kt
@@ -0,0 +1,60 @@
+package org.calypsonet.keyple.demo.validation.mock
+
+import org.eclipse.keyple.core.plugin.AbstractLocalReader
+
+/**
+ *  @author youssefamrani
+ */
+
+@Suppress("INVISIBLE_ABSTRACT_MEMBER_FROM_SUPER_WARNING")
+class MockSamReaderImpl : AbstractLocalReader(
+    "",
+    ""
+) {
+
+    override fun transmitApdu(apduIn: ByteArray?): ByteArray {
+        return apduIn ?: throw IllegalStateException("Mock no apdu in")
+    }
+
+    override fun getATR(): ByteArray? {
+        return null
+    }
+
+    override fun openPhysicalChannel() {
+    }
+
+    override fun isPhysicalChannelOpen(): Boolean {
+        return true
+    }
+
+    override fun isCardPresent(): Boolean {
+        return true
+    }
+
+    override fun checkCardPresence(): Boolean {
+        return true
+    }
+
+    override fun closePhysicalChannel() {
+    }
+
+    override fun isContactless(): Boolean {
+        return false
+    }
+
+    override fun isCurrentProtocol(readerProtocolName: String?): Boolean {
+        return true
+    }
+
+    override fun deactivateReaderProtocol(readerProtocolName: String?) {
+        // Do nothing
+    }
+
+    override fun activateReaderProtocol(readerProtocolName: String?) {
+        // Do nothing
+    }
+
+    companion object {
+        const val READER_NAME = "Mock_Sam"
+    }
+}
